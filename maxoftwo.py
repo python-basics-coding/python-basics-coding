aa5daab84bde29018312a6b231e4755fa1875533
suresh <kvpsuresh30@gmail.com>
2022-03-14 18:49:44 +0530

    maximum of two numbers

---
+++ maxoftwo.py
@@ -0,0 +1,10 @@
+#maximun of two numbers
+
+
+
+num1=eval(input('enter number 1'))
+num2=eval(input("enter number 2"))
+if (num1>num2):
+    print("number1 is greater:",num1)
+else:
+    print("number2 is greater:",num2)
