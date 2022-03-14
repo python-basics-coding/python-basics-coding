c6babc15403170bf094b4194d88d1e4a84a4b17d
suresh <kvpsuresh30@gmail.com>
2022-03-14 18:44:57 +0530

    adding two numbers program

---
+++ addnum.py
@@ -0,0 +1,14 @@
+#adding two numbers in python
+
+a=10
+b=20
+c=a+b
+print("your sum is :",c)
+
+
+#getting input from users and add 
+
+d=eval(input("enter number 1"))
+e=eval(input("enter number 2"))
+sum=d+e
+print("your sum is :",sum)
