62b616fdb227bb1ca9138ac6bfb2a61de073d8ab
suresh <kvpsuresh30@gmail.com>
2022-03-17 14:29:14 +0530

    add or even

---
+++ evenodd.py
@@ -0,0 +1,10 @@
+#add or even
+
+
+num=eval(input("enter your number"))
+if num%2==0:
+    print("number is even")
+elif num%2!=0:
+    print("your number is odd")
+else:
+    print("plz enter your number")
