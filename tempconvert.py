68a4f0da041e9e134481144d5022009503ce54d3
suresh <kvpsuresh30@gmail.com>
2022-03-17 14:44:21 +0530

    temperature program

---
+++ tempconvert.py
@@ -0,0 +1,5 @@
+#convert temperature from centigrade to farenheit
+
+centi=eval(input("enter the temperature"))
+conv= (centi*8) +32
+print('the temperature of {} converted to {}'.format(centi,conv))
